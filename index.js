0bc6f6fa4bb812eb587a512101d3605e7e894895
jessicahylee <jehyerinn@gmail.com>
2023-11-17 20:54:44 -0500

    Google OAuth

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const expressSession = require('express-session')
 const cors = require('cors')
 const helmet = require('helmet')
 const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const knex = require('knex')(require('./knexfile.js'))
-
-// const GoogleStrategy = require('passport-google-oauth20')
-require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT || 5050
 
+require('dotenv').config()
 
 app.use(express.json())
 
@@ -32,41 +31,57 @@ app.use(
     saveUninitialized: true,
   }),
 )
-
-
-
+app.use(passport.initialize())
 // const authRoutes = require('./routes/auth')
-
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-
-
+app.use(passport.session())
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      scope: ['profile'],
     },
-    function verify(accessToken, refreshToken, profile, cb) {
-      // If e-mail in the profile exist in db then trhow an error returning the code
-      // cb -> return cb(error, null)
-
-      // client is not in db then create the client and token and return cb(null,{token:"this is where token goes"})
-      // console.log(cb)
-      return cb(null, profile)
+    function (_accessToken, _refreshToken, profile, done) {
+      console.log(profile)
+      knex('users')
+        .select('id')
+        .where({ google_id: profile.id })
+        .then((user) => {
+          if (user.length) {
+            // If user is found, pass the user object to serialize function
+            done(null, user[0])
+          } else {
+            // If user isn't found, we create a record
+            knex('users')
+              .insert({
+                google_id: profile.id,
+                avatar_url: profile._json.picture,
+                username: profile.displayName,
+              })
+              .then((userId) => {
+               console.log(userId)
+                done(null, { id: userId[0] })
+              })
+              .catch((err) => {
+                console.log('Error creating a user', err)
+              })
+          }
+        })
+        .catch((err) => {
+          console.log('Error fetching a user', err)
+        })
     },
   ),
 )
-app.get('/auth/google', passport.authenticate('google',{scope:["profile"]}))
-app.get('/auth/google/callback',passport.authenticate('google', {
-    // successRedirect: process.env.CLIENT_URL,
-    failureRedirect: `${process.env.CLIENT_URL}/auth-fail`,
-    session: false,
-  }),
-  (_req, res, done) => {
-    res.redirect(process.env.CLIENT_URL)
-    return done()
-  }, ) 
+
+passport.serializeUser(function (user, done) {
+  console.log('serializeUser (user object):', user)
+  done(null, user.id)
+})
+
+const authRoutes = require('./routes/auth')
+app.use('/auth', authRoutes)
 
 app.listen(PORT, () => {
   console.log(`🚀 Server listening on port ${5050}.`)
--- routes/auth.js
+++ routes/auth.js
@@ -1,27 +1,8 @@
-const express = require('express')
-const router = express.Router()
-const passport = require('passport')
-require('dotenv').config()
-const GoogleStrategy = require('passport-google-oauth20')
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+require('dotenv').config();
 
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
-    },
-    function verify(accessToken, refreshToken, profile, cb) {
-      // If e-mail in the profile exist in db then trhow an error returning the code
-      // cb -> return cb(error, null)
-
-      // client is not in db then create the client and token and return cb(null,{token:"this is where token goes"})
-      console.log(cb)
-      return cb(null, profile)
-    },
-  ),
-)
 
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
@@ -29,7 +10,7 @@ router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 router.get(
   '/google/callback',
   passport.authenticate('google', {
-    // successRedirect: process.env.CLIENT_URL,
+    successRedirect: process.env.CLIENT_URL,
     failureRedirect: `${process.env.CLIENT_URL}/auth-fail`,
     session: false,
   }),
@@ -45,15 +26,15 @@ router.get('/profile', (req, res) => {
   res.status(200).json(req.user)
 })
 
-// router.get('/logout', (req, res) => {
-//   req.logout((error) => {
-//     if (error) {
-//       return res
-//         .status(500)
-//         .json({ message: 'Server error, please try again later', error: error })
-//     }
-//     res.redirect(process.env.CLIENT_URL)
-//   })
-// })
+router.get('/logout', (req, res) => {
+  req.logout((error) => {
+    if (error) {
+      return res
+        .status(500)
+        .json({ message: 'Server error, please try again later', error: error })
+    }
+    res.redirect(process.env.CLIENT_URL)
+  })
+})
 
 module.exports = router
