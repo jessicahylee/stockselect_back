ad43ab9e92752de07a13fca722d9dcc682da76fe
jessicahylee <jehyerinn@gmail.com>
2024-01-15 16:52:50 -0500

    project is ready

--- index.js
+++ index.js
@@ -64,39 +64,7 @@
 //       console.log('Google Profile:', profile);
 //       console.log('User Object:', user);
 
-//       knex('users')
-//         .select('id')
-//         .where({ id: google.id })
-//         .then((existingUser) => {
-//           if (existingUser.length) {
-//             // If user is found, pass the user object to serialize function
-//             done(null, existingUser[0]);
-//           } else {
-//             // If user isn't found, we create a record
-//             knex('users')
-//               .insert({
-//                 id: google.id,
-//                 avatar_url: profile._json.avatar_url,
-//                 username: profile.username,
-//               })
-//               .returning('*') // This ensures that it returns the inserted row
-//               .then((newUser) => {
-//                 // Pass the user object to serialize function
-//                 done(null, newUser[0]);
-//               })
-//               .catch((err) => {
-//                 console.log('Error creating a user', err);
-//                 done(err, null);
-//               });
-//           }
-//         })
-//         .catch((err) => {
-//           console.log('Error fetching a user', err);
-//           done(err, null);
-//         });
-//     }
-//   )
-// );
+
 
 // // Google authentication routes
 // app.get(
@@ -184,13 +152,17 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+  done(null, user);
 });
 
-passport.deserializeUser((id, done) => {
+passport.deserializeUser((user, done) => {
   // In a real application, you would fetch user information from the database
+  console.log(user)
+  console.log('im deserializeuser')
   // Example: User.findById(id, (err, user) => done(err, user));
-  done(null, { id });
+  
+  
+  done(null, {user});
 });
 
 passport.use(
@@ -200,19 +172,62 @@ passport.use(
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
-    function (accessToken, refreshToken, profile, cb) {
+    function (accessToken, refreshToken, profile, done) {
       const user = {
         id: profile.id,
+        name: profile.displayName,
+        picture: profile.photos[0].value,
         // Add any other user information you want to store in the user object
       };
 
       console.log('Google Profile:', profile);
       console.log('User Object:', user);
-
-      return cb(null, user);
-    },
-  ),
+      knex('users')
+        .select('id')
+        .select('avatar_url')
+        .select('username')
+        .where({ google_id: profile.id })
+        .then((existingUser) => {
+          if (existingUser.length) {
+            console.log('im here');
+            // If user is found, pass the user object to serialize function
+            console.log(existingUser[0])
+            done(null, existingUser[0]); 
+            
+          } else {
+            console.log('im here 2')
+            // If user isn't found, we create a record
+            knex('users')
+              .insert({
+                google_id: profile.id,
+                avatar_url: profile._json.picture,
+                username: profile.displayName,
+              })
+              .returning('*') // This ensures that it returns the inserted row
+              .then((newUser) => {
+                console.log('im here3')
+                // Pass the user object to serialize function
+                done(null, newUser[0]);
+              })
+              .catch((err) => {
+                console.log('Error creating a user', err);
+                done(err, null);
+              });
+          }
+        })
+        .catch((err) => {
+          console.log('Error fetching a user', err);
+          done(err, null);
+        });
+    }
+  )
 );
+//       return cb(null, user);
+//     },
+//   ),
+// );
+
+
 // Google authentication routes
 app.get(
   '/auth/google',
@@ -250,7 +265,24 @@ app.post('/auth/logout', (req, res) => {
 app.get('/auth/profile', (req, res) => {
   if (req.isAuthenticated()) {
     console.log('User is authenticated');
-    res.status(200).json(req.user);
+    // console.log(req.user)
+    // console.log(res) // Inseert in table (Database)
+    knex('users')
+    .where({ id: req.user.id })
+    .then(user => {
+      // Remember that knex will return an array of records, so we need to get a single record from it
+      console.log('req.user:', user[0]);
+
+      // The full user object will be attached to request object as `req.user`
+      // done(null, user[0]);
+      // res.json(user[0])
+      res.status(200).json(user[0]);
+    })
+    .catch(err => {
+      console.log('Error finding user', err);
+        res.status(401).json({ message: 'Error finding user' });
+    });
+  
   } else {
     console.log('User is not authenticated');
     res.status(401).json({ message: 'Unauthorized' });
