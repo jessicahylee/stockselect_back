82f9286570a647127a2d991b351ea49545a4b2b2
jessicahylee <jehyerinn@gmail.com>
2024-01-17 23:24:18 -0500

    ready to deploy

--- index.js
+++ index.js
@@ -1,118 +1,3 @@
-// const express = require('express');
-// const session = require('express-session');
-// const cors = require('cors');
-// const helmet = require('helmet');
-// const passport = require('passport');
-// const GoogleStrategy = require('passport-google-oauth20').Strategy;
-// const knex = require('knex')(require('./knexfile.js'));
-// require('dotenv').config();
-// require('./passport.js');
-
-// const app = express();
-// const PORT = process.env.PORT || 5050;
-
-// app.use(express.json());
-// app.use(helmet());
-
-// app.use(
-//   cors({
-//     origin: 'http://localhost:5173',
-//     credentials: true,
-//   }),
-// );
-
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: true,
-//     saveUninitialized: true,
-//   }),
-// );
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// passport.serializeUser((user, done) => {
-//   done(null, user.id);
-// });
-
-
-// passport.deserializeUser((id, done) => {
-//   knex('users')
-//     .where({ id: id })
-//     .then((user) => {
-//       done(null, user[0]);
-//     })
-//     .catch((err) => {
-//       done(err, user.id);
-//     });
-// });
-
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//       callbackURL: process.env.GOOGLE_CALLBACK_URL,
-//     },
-//     function (accessToken, refreshToken, profile, done) {
-//       const user = {
-//         id: profile.id,
-//         // Add any other user information you want to store in the user object
-//       };
-
-//       console.log('Google Profile:', profile);
-//       console.log('User Object:', user);
-
-
-
-// // Google authentication routes
-// app.get(
-//   '/auth/google',
-//   (req, res, next) => {
-//     console.log('Starting Google authentication process');
-//     passport.authenticate('google', {
-//       scope: ['profile'],
-//       prompt: 'select_account',
-//     })(req, res, next);
-//   }
-// );
-
-// app.get(
-//   '/auth/google/callback',
-//   (req, res, next) => {
-//     console.log('Handling Google callback');
-//     passport.authenticate('google', {
-//       failureRedirect: 'http://localhost:5173/login',
-//     })(req, res, next);
-//   },
-//   (_req, res) => {
-//     console.log('Redirecting to profile route');
-//     res.redirect('http://localhost:5173/profile');
-//   },
-// );
-
-// // Logout route
-// app.post('/auth/logout', (req, res) => {
-//   req.logout();
-//   req.session.destroy();
-//   res.status(200).json({ message: 'Logout successful' });
-// });
-
-// // Profile route
-// app.get('/auth/profile', (req, res) => {
-//   if (req.isAuthenticated()) {
-//     console.log('User is authenticated');
-//     res.status(200).json(req.user);
-//   } else {
-//     console.log('User is not authenticated');
-//     res.status(401).json({ message: 'Unauthorized' });
-//   }
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server listening on port ${PORT}.`);
-// });
 
 
 
@@ -135,7 +20,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.ORIGIN_URL,
     credentials: true,
   }),
 );
@@ -245,12 +130,12 @@ app.get(
   (req, res, next) => {
     console.log('Handling Google callback');
     passport.authenticate('google', {
-      failureRedirect: 'http://localhost:5173/login',
+      failureRedirect: process.env.FAILURE_URL,
     })(req, res, next);
   },
   (_req, res) => {
     console.log('Redirecting to profile route');
-    res.redirect('http://localhost:5173/profile');
+    res.redirect(process.env.REDIRECT_URL)
   },
 );
 
@@ -282,7 +167,6 @@ app.get('/auth/profile', (req, res) => {
       console.log('Error finding user', err);
         res.status(401).json({ message: 'Error finding user' });
     });
-  
   } else {
     console.log('User is not authenticated');
     res.status(401).json({ message: 'Unauthorized' });
--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,26 @@
-module.exports = {
-  client: 'mysql',
+require('dotenv').config();
+
+
+// module.exports = {
+//   client: 'mysql',
+//   connection: {
+//     host: '127.0.0.1',
+//     user: 'root',
+//     password: 'Salam2020!',
+//     database: 'stock-select',
+//     charset: 'utf8',
+//   },
+// }
+module.exports={
+  client: "mysql2",
   connection: {
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'Salam2020!',
-    database: 'stock-select',
+    host: process.env.DATABASEHOST,
+    user: process.env.DATABASEUSERNAME,
+    password: process.env.DATABASEPASSWORD,
+    database: process.env.DATABASENAME,
     charset: 'utf8',
-  },
+  }
+  
 }
+
+
